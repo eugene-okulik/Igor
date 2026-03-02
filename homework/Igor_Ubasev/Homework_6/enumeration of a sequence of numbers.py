b933f09d9f013d7a4dea0e80b248145a0c44a5c9
Igor Ubasev <igor.ubasev@gmail.com>
2026-03-02 17:37:43 +0300

    Домашнее задание по 6 уроку

---
+++ homework/Igor_Ubasev/Homework_6/enumeration of a sequence of numbers.py
@@ -0,0 +1,17 @@
+number = range(1, 101)
+results = []
+for num in number:
+    # print(num)
+    if num % 3 == 0 and num % 5 == 0:
+        print("FuzzBuzz")
+    elif num % 3 == 0:
+        print("Fuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+
+
+
+
