79f9cc49bf3ee906c4a25a32c4dfdcd208008771
Igor Ubasev <igor.ubasev@gmail.com>
2026-03-04 15:32:55 +0300

    Домашнее задание по 7 уроку исправил файл_2

--- homework/Igor_Ubasev/Homework_7/Task_3.py
+++ homework/Igor_Ubasev/Homework_7/Task_3.py
@@ -10,8 +10,12 @@ def process_line(line):
     colon_pos = line.index(':')
     number = int(line[colon_pos + 1:].strip())
     return number + 10
+
+
 results = []
 for line in lines:
     results.append(process_line(line))
+
+    
 for result in results:
     print(result)
