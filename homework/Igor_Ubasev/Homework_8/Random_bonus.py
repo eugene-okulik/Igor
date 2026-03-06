fb7675a078103c45d91e5e9d3c3e90d12a97163d
Igor Ubasev <igor.ubasev@gmail.com>
2026-03-06 15:29:54 +0300

    Домашнее задание по 8 уроку_исправил файл

--- homework/Igor_Ubasev/Homework_8/Random_bonus.py
+++ homework/Igor_Ubasev/Homework_8/Random_bonus.py
@@ -2,5 +2,5 @@ import random
 
 salary = int(input("Какая у вас зарплата: "))
 bonus = random.choice([True, False])
-if True == bonus :
-    print(f"${salary+random.randint(1, 10000)}")
+if bonus is True:
+    print(f"${salary + random.randint(1, 10000)}")
